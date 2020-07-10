55fae0765e41e1052934128c6844d7437bfe5800
Twinf0x <bastian.7741@gmail.com>
2020-07-11 00:35:24 +0200

    Implemented basic movement as part of the player health system

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovementDirection { Up, Down, Left, Right }
+
+public class PlayerMovement : PlayerHealthComponent
+{
+    #region Inspector
+    [SerializeField] private MovementDirection direction;
+    [SerializeField] private KeyCode[] viableInputs;
+    #endregion
+
+    public Vector2 Value { get; private set; }
+
+    private void Update()
+    {
+        foreach(var key in viableInputs)
+        {
+            if (Input.GetKey(key))
+            {
+                Value = GetDirection(direction);
+                return;
+            }
+        }
+
+        Value = Vector2.zero;
+    }
+
+    private Vector2 GetDirection (MovementDirection direction)
+    {
+        switch (direction)
+        {
+            case MovementDirection.Up:
+                return new Vector2(0f, 1f);
+            case MovementDirection.Down:
+                return new Vector2(0f, -1f);
+            case MovementDirection.Left:
+                return new Vector2(-1f, 0f);
+            case MovementDirection.Right:
+                return new Vector2(1f, 0f);
+        }
+
+        throw new System.Exception("Invalid movement direction");
+    }
+}
