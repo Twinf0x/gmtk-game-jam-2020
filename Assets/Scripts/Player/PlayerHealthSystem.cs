15a2b7378cdf7c4bc96a24b29c7b628fe2283c40
Twinf0x <bastian.7741@gmail.com>
2020-07-11 11:51:25 +0200

    Reworked health system, so it searches for health components automatically on start so you don't have to f*** around with all those components in the inspector

--- Assets/Scripts/Player/PlayerHealthSystem.cs
+++ Assets/Scripts/Player/PlayerHealthSystem.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerHealthSystem : MonoBehaviour
 {
-    [SerializeField] private PlayerHealthComponent[] movementComponents;
-    [SerializeField] private PlayerWeapon[] weaponComponents;
+    [SerializeField] private PlayerWeapon fallbackWeapon;
+
+    private List<PlayerWeapon> activeWeaponComponents;
+    private List<PlayerMovement> activeMovementComponents;
+
+    private void Start()
+    {
+        activeMovementComponents = gameObject.GetComponents<PlayerMovement>().ToList();
+        activeWeaponComponents = gameObject.GetComponents<PlayerWeapon>().Where(weapon => weapon != fallbackWeapon).ToList();
+    }
+
+    public void DeactivateRandomComponent()
+    {
+        if(activeWeaponComponents.Count > 0)
+        {
+            var weapon = activeWeaponComponents.ElementAt(Random.Range(0, activeWeaponComponents.Count));
+            activeWeaponComponents.Remove(weapon);
+            weapon.enabled = false;
+        }
+
+        if(activeMovementComponents.Count > 0)
+        {
+            var movement = activeMovementComponents.ElementAt(Random.Range(0, activeMovementComponents.Count));
+            activeMovementComponents.Remove(movement);
+            movement.enabled = false;
+        }
+
+        Die();
+    }
 
     public PlayerWeapon GetRandomActiveWeapon()
     {
-        var index = Random.Range(0, weaponComponents.Length);
+        if(activeWeaponComponents.Count < 0)
+        {
+            return fallbackWeapon;
+        }
 
-        return weaponComponents[index];
+        var index = Random.Range(0, activeWeaponComponents.Count);
+        return activeWeaponComponents.ElementAt(index);
+    }
+
+    public void Die()
+    {
+        //TODO
     }
 }
