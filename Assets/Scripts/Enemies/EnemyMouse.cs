59ff96a47ea39200e14cffc1c560481d0ea26fda
Markus Zoppelt <markus@zoppelt.net>
2020-07-11 20:08:36 +0200

    started working on enemies

---
+++ Assets/Scripts/Enemies/EnemyMouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMouse : MonoBehaviour
+{
+   [SerializeField] internal Rigidbody2D body;
+   [SerializeField] internal EnemyHealth health;
+   [SerializeField] internal Vector2 position;
+   [SerializeField] internal Vector2 movement;
+   [SerializeField] internal float movementSpeed;
+   [SerializeField] internal Transform target;
+
+   internal void Start() {
+       // call out loud
+       movementSpeed = 2f;
+   }
+
+   internal virtual void FixedUpdate() {
+       var playerdirection = target.position - transform.position;
+       Move(playerdirection.normalized);
+   }
+
+   private void Move(Vector2 direction) {
+       body.velocity = direction * movementSpeed;
+    }
+}
