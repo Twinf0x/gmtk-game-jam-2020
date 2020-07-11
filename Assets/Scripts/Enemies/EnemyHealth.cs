6186ab75842875bbf2efa57aabc62dcea2a25676
Twinf0x <bastian.7741@gmail.com>
2020-07-11 15:02:54 +0200

    Added basic enemy health. Implemented rocket explosions

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float maxHealth;
+    [SerializeField] private GameObject corpsePrefab;
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Instantiate(corpsePrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
