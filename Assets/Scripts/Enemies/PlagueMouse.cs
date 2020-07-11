7e977a3ef7f83b957c6aee91168570d4ab7f1288
Markus Zoppelt <markus@zoppelt.net>
2020-07-11 20:29:07 +0200

    enemy classtypes

--- Assets/Scripts/Enemies/PlagueMouse.cs
+++ Assets/Scripts/Enemies/PlagueMouse.cs
@@ -2,37 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArmedMouse : EnemyMouse
+public class PlagueMouse : EnemyMouse
 {
     private const float bulletSpeed = 3f;
     [SerializeField] internal Transform firePoint;
     [SerializeField] internal GameObject bulletPrefab;
-    internal float timeBetweenShots;
-    internal float timeToNextShot;
+    internal float timeBetweenSpits;
+    internal float timeToNextSpit;
 
     // Start is called before the first frame update
     void Start()
     {
-        base.movementSpeed = 1f;
+        base.movementSpeed = 2f;
     }
 
     // Update is called once per frame
     internal override void FixedUpdate() {
-        var playerdirection = transform.position - base.target.position;
+        var playerdirection = base.target.position - transform.position;
         Move(playerdirection.normalized);
-        Shoot(playerdirection);
+
+        if (base.body.position.x < 30) {
+            Spit(playerdirection);
+        }
     }
 
     private void Move(Vector2 direction) {
        base.body.velocity = direction * movementSpeed;
     }
 
-    private void Shoot(Vector2 direction) {
-        if (timeToNextShot > 0f) {
+    private void Spit(Vector2 direction) {
+        if (timeToNextSpit > 0f) {
             return;
         }
 
-        timeToNextShot = timeBetweenShots;
+        timeToNextSpit = timeBetweenSpits;
         var bulletObject = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         var bullet = bulletObject.GetComponent<Bullet>();
         bullet.direction = transform.position - base.target.position;
