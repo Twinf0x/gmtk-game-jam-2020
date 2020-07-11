5506b9ec9866fd65950467b3a90c21a0f85d6948
Markus Zoppelt <markus@zoppelt.net>
2020-07-12 00:26:58 +0200

    some fixes for laser gun

--- Assets/Scripts/Player/Weapons/LaserGun.cs
+++ Assets/Scripts/Player/Weapons/LaserGun.cs
@@ -1,23 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LaserGun : PlayerWeapon
 {
+    [SerializeField] private float damagePerSecond;
+    [SerializeField] private float castRadius;
+    [SerializeField] private float laserRange;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private LineRenderer renderer;
+
+    private bool isActive = false;
+
     internal override void Start(){
         base.Start();
     }
 
     internal override void Update() {
         base.Update();
+
+        renderer.enabled = Input.GetKey(KeyCode.Mouse0) && isActive;
     }
 
     public override void Activate() {
         base.Activate();
+
+        isActive = true;
     }
 
     public override void Fire(Vector2 direction) {
-        base.Fire(direction);
 
+        if(bulletsLeft <= 0) {
+            return;
+        }
+
+        RaycastHit2D[] greatestHits = Physics2D.CircleCastAll(firePoint.position, castRadius, direction, Mathf.Infinity, layerMask);
+
+        foreach( var hit in greatestHits) {
+            if (hit.collider) {
+                var hitTarget = hit.collider.gameObject.GetComponent<EnemyHealth>();
+                if (hitTarget) {
+                    hitTarget.TakeDamage(damagePerSecond * Time.deltaTime);
+                }
+            }
+        }
+
+        renderer.SetPosition(0, firePoint.position);
+        renderer.SetPosition(1, firePoint.position + new Vector3(direction.x, direction.y, 0) * laserRange);
+
+        bulletsLeft--;
+        if(bulletsLeft <= 0)
+        {
+            renderer.enabled = false;
+            isActive = false;
+            Debug.Log("Switching weapons");
+            onMagazineEmpty?.Invoke();
+        }
     }
 }
